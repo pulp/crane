853b60b5133ef0b7c0efee1227337760ce23b4d6
Sean Myers <sean.myers@redhat.com>
2016-09-15 02:01:20 -0400

    Bumping version to 2.0.3-0.1.alpha

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2014-2016, Pulp Team'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0'
+version = '2.0.3a1'
 # The full version, including alpha/beta/rc tags.
 release = '2.0.3a1'
 
